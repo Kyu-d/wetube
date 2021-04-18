41894af098f8befe2b57326ceb999f51926fd3ec
Kyu-d <ldk0421@naver.com>
2021-04-19 00:00:24 +0900

    create wetube with expressjs & pug

---
+++ router/userRouter.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const userRouter = express.Router();
+
+export default userRouter;
+
+// userRouter.get("/", (req, res) => res.send('user index'));
+// userRouter.get("/edit", (req, res) => res.send('user edit'));
+// userRouter.get("/password", (req, res) => res.send('user password'));
+
+// M Data
+// V how does the data look
+// C function that looks for the data
