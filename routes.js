f9f5b20b58167dd84f3394c5fbaa0b954e5a0be4
Kyu-d <ldk0421@naver.com>
2021-04-26 02:28:01 +0900

    revise more controller - login, logout, upload...
    - detail...
    - logout...
    - upload...

--- routes.js
+++ routes.js
@@ -29,7 +29,7 @@ const routes = {
     logout: LOGOUT,
     search: SEARCH,
     users: USERS,
-    userDetail: (id) => {
+    userDetail: id => {
         if(id){
             return `/users/${id}`;
         } else {
@@ -40,7 +40,7 @@ const routes = {
     changePassword: CHANGE_PASSWORD,
     videos: VIDEOS,
     upload: UPLOAD,
-    videoDetail: (id) => {
+    videoDetail: id => {
         if(id){
             return `/videos/${id}`;
         } else {
