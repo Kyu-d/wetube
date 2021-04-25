2d170a018bab784f2f017d350ac42a85dc9fda82
Kyu-d <ldk0421@naver.com>
2021-04-25 22:22:47 +0900

    create editProfile page & changePassword page

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,13 +1,20 @@
 import express from "express";
+import routes from "../routes";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
+
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
+
 export default userRouter;
-
-// userRouter.get("/", (req, res) => res.send('user index'));
-// userRouter.get("/edit", (req, res) => res.send('user edit'));
-// userRouter.get("/password", (req, res) => res.send('user password'));
-
 // M Data
 // V how does the data look
 // C function that looks for the data
