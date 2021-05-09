8fea980d404d9c30af83bf8a9e823d05519e5425
Kyu-d <ldk0421@naver.com>
2021-05-10 01:25:25 +0900

    Revise everything related to the video

--- router/videoRouter.js
+++ router/videoRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import {
-    getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo
+    getUpload, postUpload, videoDetail, deleteVideo, getEditVideo, postEditVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
@@ -21,6 +21,6 @@ videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
 
 // Delete Video
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
