72bff4920c48c9392140eedd42760c14af2d99f7
Kyu-d <ldk0421@naver.com>
2021-05-10 00:23:43 +0900

    Revise edit video part - videoController.js : add get & post editvideo - videoRouter.js : revise editvideo > split > get / post, add Remark - videoDetail.pug : revise 'a' tag url - editVideo.pug : revise 'a' tag url, input & textarea add value

--- router/videoRouter.js
+++ router/videoRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import routes from "../routes";
 import {
-    getUpload, postUpload, videoDetail, editVideo, deleteVideo
+    getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo
 } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares";
 
@@ -9,11 +9,18 @@ const videoRouter = express.Router();
 
 // video hello finished
 
+// Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+// Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+// Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+// Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
